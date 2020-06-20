b622c3d4f555f4da0955357fbf3b349c8f5380af
gulbara <gulbara@MacBook-Pro-gulbara.local>
2020-06-19 20:54:22 -0400

    first push

---
+++ src/day_1/Enhanced_Loop_practice.java
@@ -0,0 +1,20 @@
+package day_1;
+
+public class Enhanced_Loop_practice {
+
+    public static void main(String[] args) {
+        int[] numbers = new int [] {1,2,3,4,5,6,7};
+
+        for (int eachNumber:numbers) {
+            System.out.println(eachNumber);}
+
+
+
+    }
+
+
+
+
+
+
+}
